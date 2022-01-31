fceeea25c883585d4b9c207a7cdd4855d5b1623e
usu2 <usu2@prueba.com>
2022-01-31 08:27:10 +0100

    validamos cambio2 en proyecto2

---
+++ prueba1/src/prueba1/Cambio2.java
@@ -0,0 +1,8 @@
+package prueba1;
+
+public class Cambio2 {
+	public static void main(String[] args) {
+		System.out.println("clase cambio2 en proyrecto2");
+		System.out.println("clase cambio2 en proyrecto2");
+	}
+}
